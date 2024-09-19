2e8e2f5d2db191f0da44013b5d208cab688050ff
alexleoncoeur <alejandroarenasleon2@gmail.com>
2024-09-19 17:50:39 +0200

    exit modified

--- src/built_ins/exit.c
+++ src/built_ins/exit.c
@@ -6,7 +6,7 @@
 /*   By: aarenas- <aarenas-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 13:08:08 by aarenas-          #+#    #+#             */
-/*   Updated: 2024/09/19 13:23:52 by aarenas-         ###   ########.fr       */
+/*   Updated: 2024/09/19 17:49:55 by aarenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,10 @@
 void	ft_exit(int error) //o error guardado en estructura
 {
 	//manejar errores especificos de exit
+	if (ft_isdigit(error) != 1)
+		error = 2;
+
+	if (error != 0)
+		ft_write_error(error);
 	exit(error);
 }
