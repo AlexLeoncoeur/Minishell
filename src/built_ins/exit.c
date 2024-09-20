c06447f594875df6a69654a1d0f8f842e2bc58f8
alexleoncoeur <alejandroarenasleon2@gmail.com>
2024-09-20 12:17:11 +0200

    Minor change in exit

--- src/built_ins/exit.c
+++ src/built_ins/exit.c
@@ -6,7 +6,7 @@
 /*   By: aarenas- <aarenas-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 13:08:08 by aarenas-          #+#    #+#             */
-/*   Updated: 2024/09/19 17:49:55 by aarenas-         ###   ########.fr       */
+/*   Updated: 2024/09/20 10:53:28 by aarenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	ft_exit(int error) //o error guardado en estructura
 	//manejar errores especificos de exit
 	if (ft_isdigit(error) != 1)
 		error = 2;
-
+	//si el primer agumento es un numero y hay mas arrrrrrgumentos, no sale. too many arguments 1
+	//si el primero es letra, sale pero da error numeric argument required 2
 	if (error != 0)
 		ft_write_error(error);
 	exit(error);
