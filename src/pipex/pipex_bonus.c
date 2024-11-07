213515cadd20e1cab864b19875d0d938e18d5a76
alexleoncoeur <alejandroarenasleon2@gmail.com>
2024-11-07 16:15:28 +0100

    executer ready

--- src/pipex/pipex_bonus.c
+++ src/pipex/pipex_bonus.c
@@ -6,58 +6,11 @@
 /*   By: aarenas- <aarenas-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 10:39:13 by aarenas-          #+#    #+#             */
-/*   Updated: 2024/11/07 15:28:29 by aarenas-         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:15:07 by aarenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-static char	*ft_definitive_path(char **d_paths, char *command)
-{
-	char	*endl;
-	char	*path;
-	int		i;
-
-	i = 0;
-	/* if (lst->flags)
-		ft_free(lst->flags); */
-	//lst->flags = ft_split(lst->argv[pos], ' ');
-	i = 0;
-	endl = ft_strjoin("/", command);
-	path = ft_strjoin(d_paths[0], endl);
-	while (d_paths[i] && access(path, X_OK) < 0)
-	{
-		free(path);
-		path = ft_strjoin(d_paths[i], endl);
-		i++;
-	}
-	if (d_paths[i])
-		return (free(endl), path);
-	free(path);
-	return (free(endl), NULL);
-}
-
-static char	*ft_pathfinder(t_arg_list *lst, char *command)
-{
-	char	**d_paths;
-	char	*path;
-	int		i;
-
-	i = 0;
-	while (ft_strncmp(lst->envp[i], "PATH=", 5) != 0)
-		i++;
-	if (lst->envp[i] == NULL)
-		return (NULL);
-	path = ft_substr(lst->envp[i], 5, ft_strlen(lst->envp[i]) - 5);
-	if (path == NULL)
-		return (NULL);
-	d_paths = ft_split(path, ':');
-	free(path);
-	path = ft_definitive_path(d_paths, command);
-	if (!path)
-		ft_puterrorstr("Error: Command not found\n");
-	return (path);
-}
 
 static void	ft_execute_cmd(t_cmd *cmd, int *pipefd, int i, int *builtin_done)
 {
@@ -116,51 +69,8 @@ void	ft_do_cmd(t_arg_list *lst)
 	}
 }
 
-static t_cmd	*ft_test_cmd(t_arg_list *data)
+int	ft_executer(t_arg_list *data)
 {
-	t_cmd	*prueba;
-	char	*arg;
-
-	prueba = malloc(sizeof(t_cmd));
-	arg = ft_strdup("..");
-	prueba->argv = malloc(sizeof(char *) * 2);
-	prueba->next->argv[1] = NULL;
-	printf("pipo por favor\n");
-	prueba->next->argv[0] = arg;
-	prueba->envp = data->envp;
-	prueba->redir = -1;
-	prueba->next = malloc(sizeof(t_cmd));
-	prueba->env = NULL;
-	prueba->env_export = NULL;
-	prueba->data = data;
-	prueba->next->argv = NULL;
-/* 	prueba->next->argv[1] = NULL;
-	prueba->next->argv[0] = arg; */
-	prueba->next->envp = data->envp;
-	prueba->next->redir = -1;
-	prueba->next->next = NULL;
-	prueba->next->env = NULL;
-	prueba->next->env_export = NULL;
-	prueba->data = data;
-	return (prueba);
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	t_arg_list	*data;
-	//t_arg_list	*lst;
-
-	//lst = ft_define_lst(argc, argv, envp);
-	data = malloc(sizeof(t_arg_list));
-	data->argc = argc;
-	data->argv = argv;
-	data->envp = envp;
-	data->cmd = ft_test_cmd(data);
-	data->cmd->cmd = ft_pathfinder(data, "cd");
-	data->builtin_done = 0;
-	data->cmd->next->cmd = "pwd";
-	//data->cmd->next->cmd = ft_pathfinder(data, "exit");
-	//data->cmd->next->argv[0] = data->cmd->next->cmd;
 	/* if (data->cmd->redir == -1)
 		exit(1); */
 	if (data->cmd && !data->cmd->next)
@@ -192,3 +102,95 @@ int	main(int argc, char **argv, char **envp)
 //	que lo guarde en la estructura t_data
 //No es necesario gestionar la falta de variables de entorno, si no hay
 //	da error y listo
+
+/*testing*/
+
+/* static char	*ft_definitive_path(char **d_paths, char *command)
+{
+	char	*endl;
+	char	*path;
+	int		i;
+
+	i = 0;
+	if (lst->flags)
+		ft_free(lst->flags);
+	//lst->flags = ft_split(lst->argv[pos], ' ');
+	i = 0;
+	endl = ft_strjoin("/", command);
+	path = ft_strjoin(d_paths[0], endl);
+	while (d_paths[i] && access(path, X_OK) < 0)
+	{
+		free(path);
+		path = ft_strjoin(d_paths[i], endl);
+		i++;
+	}
+	if (d_paths[i])
+		return (free(endl), path);
+	free(path);
+	return (free(endl), NULL);
+}
+
+static char	*ft_pathfinder(t_arg_list *lst, char *command)
+{
+	char	**d_paths;
+	char	*path;
+	int		i;
+
+	i = 0;
+	while (ft_strncmp(lst->envp[i], "PATH=", 5) != 0)
+		i++;
+	if (lst->envp[i] == NULL)
+		return (NULL);
+	path = ft_substr(lst->envp[i], 5, ft_strlen(lst->envp[i]) - 5);
+	if (path == NULL)
+		return (NULL);
+	d_paths = ft_split(path, ':');
+	free(path);
+	path = ft_definitive_path(d_paths, command);
+	if (!path)
+		ft_puterrorstr("Error: Command not found\n");
+	return (path);
+} */
+
+/* static t_cmd	*ft_test_cmd(t_arg_list *data)
+{
+	t_cmd	*prueba;
+	char	*arg;
+
+	prueba = malloc(sizeof(t_cmd));
+	arg = ft_strdup("..");
+	prueba->argv = malloc(sizeof(char *) * 2);
+	prueba->argv[1] = NULL;
+	prueba->argv[0] = arg;
+	prueba->envp = data->envp;
+	prueba->redir = -1;
+	prueba->next = malloc(sizeof(t_cmd));
+	prueba->env = NULL;
+	prueba->env_export = NULL;
+	prueba->data = data;
+	prueba->next->argv = NULL;
+	prueba->next->argv[1] = NULL;
+	prueba->next->argv[0] = arg;
+	prueba->next->envp = data->envp;
+	prueba->next->redir = -1;
+	prueba->next->next = NULL;
+	prueba->next->env = NULL;
+	prueba->next->env_export = NULL;
+	prueba->data = data;
+	return (prueba);
+} */
+
+	/* t_arg_list	*data;
+	//t_arg_list	*lst;
+
+	//lst = ft_define_lst(argc, argv, envp);
+	data = malloc(sizeof(t_arg_list));
+	data->argc = argc;
+	data->argv = argv;
+	data->envp = envp;
+	data->cmd = ft_test_cmd(data);
+	data->cmd->cmd = "cd";
+	data->builtin_done = 0;
+	data->cmd->next->cmd = "pwd";
+	//data->cmd->next->cmd = ft_pathfinder(data, "exit");
+	//data->cmd->next->argv[0] = data->cmd->next->cmd; */
